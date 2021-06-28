8656beca8990488c18be3d89c1587bd00326f152
zeqas <casperi52400@gmail.com>
2021-06-29 01:29:02 +0800

    新增font awesome

--- app.js
+++ app.js
@@ -10,18 +10,21 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurant: restaurantList.results})
+  res.render('index', { restaurants: restaurantList.results})
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) =>{
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant.restaurant_id)
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant : restaurant})
 })
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name_en.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()) || restaurant.name.includes(keyword)
+    return restaurant.name_en.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()) || 
+    restaurant.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()) || 
+    restaurant.category.toLocaleLowerCase().includes(keyword.toLocaleLowerCase())
+
   })
 
   res.render('index', { restaurants : restaurants, keyword : keyword})
