fb7a00d89b8c317003f06808740633107ad74f82
zeqas <casperi52400@gmail.com>
2021-08-03 17:33:19 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -33,9 +33,12 @@ app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
 
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
