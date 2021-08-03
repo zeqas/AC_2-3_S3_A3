e035e69a3e7918686a2ad1b89e08817e3aafde84
zeqas <casperi52400@gmail.com>
2021-08-03 10:44:55 +0800

    feat: add user login and registation feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -14,6 +15,12 @@ app.engine('hbs', exphbs({
   helpers: require('./controller/handlebarHelpers')
 }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
---
+++ routes/modules/users.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../../models/user')
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/register', (req, res) => {
+
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('User already exists.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
+})
+
+module.exports = router
